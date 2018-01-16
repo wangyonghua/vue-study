314591544581971a5808061adacbd8d914b47887
wangyonghua <yonghuawang3@creditease.cn>
2018-01-16 17:05:20 +0800

    add code

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/static/',
+        filename: 'build.js'
+    },
+    module: {
+     rules: [
+        {
+            test: /\.vue$/,
+            use: ["vue-loader"]
+        }
+    ]
+  }
+}
+
